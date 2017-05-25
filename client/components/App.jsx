4fed6419452874d127cf5d1e538d5e8202418702
EDA <eda@Mac-Mini-4.local>
2017-05-26 10:35:42 +1200

    did something

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
-import {getWeather} from '../api'
 
 // import Header from './Header'
 // import Results from './Results'
 import Search from './Search'
-// import {getWeather} from '../api'
+import {getWeather} from '../api'
 
 class App extends React.Component{
   constructor(props){
     super(props)
 
     this.state = {
-      lat: 0,
-      long: 0,
       displayResult: false,
       displaySearch: true
     }
-    // this.getLocation = this.getLocation.bind(this)
-    // this.getLocale = this.getLocale.bind(this)
+
     this.handleSearchClick = this.handleSearchClick.bind(this)
   }
 
