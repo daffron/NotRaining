73354d8de2e3252168d4da32d19c4c90a6f452cb
EDA <eda@mac-mini-2.local>
2017-05-26 11:53:56 +1200

    add 3 day forecast to results component

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -7,13 +7,29 @@ export default (props) => {
       <h1>{props.weatherToday.city}</h1>
       <h2>Today's Weather</h2>
       <div className='today'>
-        {props.weatherToday.temp}<br />
-        {props.weatherToday.description}<br />
-        <img src={iconURL} /><br />
-        <p>Min: {props.weatherToday.low}<br /></p>
-        <p>Max: {props.weatherToday.high}</p>
+        <div className="temperature">
+        {props.weatherToday.temp}
+        </div>
+        <div className="description">
+        {props.weatherToday.description}</div>
+        <div className="icon"><img src={iconURL} /></div>
+        <div className="templow">
+        Min: {props.weatherToday.low}</div>
+        <div className="temphigh">
+        Max: {props.weatherToday.high}</div>
       </div>
       <h2>3 Day Forecast</h2>
+      <div className="multiple">
+       {futureWeather.map((day) => {
+         return 
+         <div className={futureWeather.day}>
+         {futureWeather.day}
+         {futureWeather.temperature}
+        {futureWeather.min}
+        {futureWeather.max}
+         </div>
+       })}
+      </div>
     </div>
   )
 }
