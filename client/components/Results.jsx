ecfc193bc1765d091a32bb9e02b9263daf793e2f
EDA <eda@mac-mini-2.local>
2017-05-26 12:28:56 +1200

    Added day of Week

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default (props) => {
+
   let iconURL = `http://openweathermap.org/img/w/${props.weatherToday.icon}.png`
   return (
     <div className='results'>
@@ -20,14 +21,22 @@ export default (props) => {
       </div>
       <h2>3 Day Forecast</h2>
       <div className="multiple">
+      <div className="day-title">
+      <div>T</div>
+      <div></div>
+      <div></div>
+      </div>
        {props.futureWeather.map((day) => {
-         console.log(day)
+         const a = new Date(day.date * 1000)
+         const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+         const dayofWeek = days[a.getDay()]
          return (
          <div className="day">
-         {day.temp}
-         {day.description}
-        {day.icon}
-        {day.low}
+          {dayofWeek}
+         {day.temp}<br />
+         {day.description} <br />
+        {day.icon}<br />
+        {day.low}<br />
           {day.high}
          </div>
        )
